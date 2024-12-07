6c85ffb8e7787bacb6efcd14c3df417681151ebc
Berke Enes Aktümen <81863556+berkeenesakt@users.noreply.github.com>
2024-12-07 19:12:29 +0300

    Initialize Flutter bindings before shared preferences setup

--- lib/core/init/app_init.dart
+++ lib/core/init/app_init.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:todolist/services/shared_prefs_service.dart';
 
 class AppInit {
   static Future<void> init() async {
+    WidgetsFlutterBinding.ensureInitialized();
     await SharedPrefsService().init();
   }
 }
